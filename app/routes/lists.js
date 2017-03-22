53da6e4fd35188c5ebce0ad71fb93a98695ead03
laurpaik <laurpaik@gmail.com>
2017-03-22 16:45:44 -0400

    Implements edit list
    - Shopping-list/card component:
        - add edit button
        - send action up
    
    - Lists route:
        - handle edit action, transition to list.edit route
    
    - List-edit route:
        - add list-edit route to router
        - handle edit action: persist data & transition back to routes list
        - template uses the shopping-list/edit component

--- app/routes/lists.js
+++ app/routes/lists.js
@@ -4,4 +4,9 @@ export default Ember.Route.extend({
   model () {
     return this.get('store').findAll('list');
   },
+  actions: {
+    editList (list) {
+      this.transitionTo('list-edit', list);
+    },
+  },
 });

f762d171284b465894f03a8204989686eba99605
laurpaik <laurpaik@gmail.com>
2017-03-22 16:29:05 -0400

    Un-nests edit from list

--- app/router.js
+++ app/router.js
@@ -8,7 +8,15 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('lists');
-  this.route('list', { path: '/lists/:list_id'});
+  this.route('list', { path: '/lists/:list_id'}, function() {
+
+  });
+  this.route('list-edit', { path: 'lists/:list_id/edit' });
+  // Ember trivia! dots are for nested routes! use a dash!
+  // this path is incidental! the entire point is to map the url to an end-view-state
+  // line 14 is a route definition
+  // the first argument is the url of the route definition
+  // the second argument is the path for the url of the route definition
 });
 
 export default Router;

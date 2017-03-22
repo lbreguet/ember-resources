4c75dd03fa822eba7bafcbc01d48f8d2e5d93ae3
laurpaik <laurpaik@gmail.com>
2017-03-22 12:21:29 -0400

    Persists changes in item to the database
    - Changes listItemCompleted to be an alias for item.done
    - Refactors the toggleDone action to send the action up with the item
    - Sends the toggleDone action up to the route (as toggleItemDone)
    - Persist changes to the API in the route toggleItemDone action

--- app/routes/list.js
+++ app/routes/list.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.get('store').findRecord('list', params.list_id);
+  },
+  actions: {
+    toggleItemDone(item) {
+      item.toggleProperty('done');
+      item.save();
+    }
   }
 });
